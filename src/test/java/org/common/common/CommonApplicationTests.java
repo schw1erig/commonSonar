5532a6f1ae6c395723bde04c676fcdde1cf1b4db
Darkress <github@darkress.xyz>
2022-10-18 00:02:15 +0200

    Initial commit

---
+++ src/test/java/org/common/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.common.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
